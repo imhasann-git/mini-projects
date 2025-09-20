a58e453697909cc41220355005a388cc1738089c
XisEnine <mdnauman@proton.me>
2025-09-20 19:34:35 +0530

    setup 0 done with db setup as well

---
+++ src/Linktree_OWN/server/src/db/schema.ts
@@ -0,0 +1,20 @@
+import {
+  uuid,
+  integer,
+  pgTable,
+  varchar,
+  text,
+  timestamp,
+  pgEnum,
+} from "drizzle-orm/pg-core";
+
+export const userRoleEnum = pgEnum("user_role", ["USER", "ADMIN"]);
+
+export const usersTable = pgTable("users", {
+  id: uuid().primaryKey().defaultRandom(),
+  name: varchar({ length: 255 }).notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+  role: userRoleEnum().notNull().default("USER"),
+  password: text().notNull(),
+  salt: text().notNull(),
+});
